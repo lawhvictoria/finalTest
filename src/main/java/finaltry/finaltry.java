0cce394007c41331085931a3b0786680e6514fb3
Victoria Law <vhlaw@calpoly.edu>
2018-02-28 00:18:56 -0800

    First Commit

---
+++ src/main/java/finaltry/finaltry.java
@@ -0,0 +1,7 @@
+package finaltry;
+
+public class finaltry {
+	public static void main(String[] args) {
+		System.out.println("Dishonor on you, dishonor on your family, dishonor on your cow!");
+	}
+}
